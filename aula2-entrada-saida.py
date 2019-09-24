b175145627446ba7c57893cfacc6efcd80a3737e
Thiago Pereira <tpereira131@gmail.com>
2019-09-24 16:42:55 -0300

    Introdução, operadores e exercicio 2

---
+++ aula2-entrada-saida.py
@@ -0,0 +1,7 @@
+nome = input("qual o seu nome? ")
+idade = input("Escreva sua idade: ")
+altura = input("escreva sua altura: ")
+
+print(type(nome), type(idade), type(altura))
+
+print(nome + " tem " + str(idade) + " anos e " + str(altura) + " de altura")
---
+++ exercicio_aula2.py
@@ -0,0 +1,16 @@
+'''
+Exercicio: Faça um formulario que pergunte
+o nome, cpf, endereco, idade, altura, e telefone
+e imprima isso em um relatorio organizado
+'''
+
+nome = input("Digite seu nome: ")
+cpf = input("Digite seu CPF: ")
+endereco = input("Digite seu endereco ")
+idade = input("Digite sua idade: ")
+altura = input("Digite sua altura ")
+telefone = input("Digite seu telefone ")
+
+print("Seu nome é ", nome, "de CPF: ", cpf, " que mora na ",
+      endereco, "e tem a idade de ", idade, " com a altura ",
+      altura, " e telefone ", telefone)
